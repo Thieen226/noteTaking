e7fbcc2e09af1707c2beacf8c9f79aa23d18029a
Thieen226 <t.ho1@my.metroed.net>
2024-03-25 09:35:43 -0700

    add

---
+++ note/note/noteApp.swift
@@ -0,0 +1,17 @@
+//
+//  noteApp.swift
+//  note
+//
+//  Created by StudentAM on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct noteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
